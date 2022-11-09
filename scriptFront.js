3c7943598e663e4a48871f6022df158bca70ca74
AaronBrs <brosseau.aaron@gmail.com>
2022-11-09 11:23:47 +0100

    Premiere version du tableau de SAM

--- scriptFront.js
+++ scriptFront.js
@@ -15,17 +15,28 @@ function displayPartie2(){
 function displayPartie3(){
     const divPartie3 = document.getElementById('partie3');
     const btnPartie3 = document.getElementById('btnPartie3');
+    const btnNouvelleLigne = document.getElementById('btnNouvelleLigne');
     if (divPartie3.style.display === 'none') {
         divPartie3.style.display = 'block';
-        btnPartie3.style.display='none';
+        btnPartie3.style.display = 'none';
+        btnNouvelleLigne.style.display = 'block';
     } else {
         divPartie3.style.display = 'none';
+        btnNouvelleLigne.style.display = 'none'
     }
 }
 
 function recupDonnees(){
-    const nom = document.getElementById('nom').value;
-    const prenom = document.getElementById('prenom').value;
-    const genre = document.querySelector('input[name="genre"]:checked').value;
-    const dateNaissance = document.getElementById('dateNaissance').value;
+//    const nom = document.getElementById('nom').value;
+//    const prenom = document.getElementById('prenom').value;
+//    const genre = document.querySelector('input[name="genre"]:checked').value;
+//    const dateNaissance = document.getElementById('dateNaissance').value;
+    const lesSalaires = document.getElementsByClassName('lesSalaires');
+    console.log(lesSalaires);
+}
+
+function nouvelleLigne(){
+    var nouvelleLigne = '<tr><td><div class="inputfield"><input type="number" min="1920" max="2025" step="1"  class="lesAnnees input"></div></td><td><div class="inputfield"><input type="number" min="0.00" max="1000000.00" step="0.01" class="lesSalaires input"></div</td><td><div class="inputfield"><div class="custom_select lesUnites"><select><option value="EUR" selected>EUR</option><option value="FRF">FRF</option></select></div></div> </td<td><div class="revenuReel lesSalairesReels"></div></td></tr>';
+    var tableau = document.getElementById('aRemplir');
+    tableau.insertAdjacentHTML("beforeend",nouvelleLigne);
 }
