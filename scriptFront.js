42fb12245c4170aeb8f3158c57fc6e3f157c7b70
AaronBrs <brosseau.aaron@gmail.com>
2022-11-18 12:19:29 +0100

    Ajout pas nb trimestres & modif CSS

--- scriptFront.js
+++ scriptFront.js
@@ -34,28 +34,6 @@ function displayPartie3(){
         btnNouvelleLigne.style.display = 'none'
     }
 }
-
-function recupDonnees(){
-//    const nom = document.getElementById('nom').value;
-//    const prenom = document.getElementById('prenom').value;
-//    const genre = document.querySelector('input[name="genre"]:checked').value;
-//    const dateNaissance = document.getElementById('dateNaissance').value;
-
-//    nbTrimestresValides
-//    nbTrimestresValidesAvantAge20
-//    nbTrimestresValidesAvantAge16
-
-//    nbEnfantsNes
-//    nbEnfantsEleves
-
-//    nbTrimestresHandicap
-
-//    const lesAnnees = document.getElementsByClassName('lesAnnees');
-//    const lesSalaires = document.getElementsByClassName('lesSalaires');
-//    const lesUnites = document.getElementsByClassName('lesUnites');
-
-}
-
 function nouvelleLigne(){
     var nouvelleLigne = '<tr><td><div class="inputfield"><input type="number" min="1920" max="2025" step="1"  class="lesAnnees input"></div></td><td><div class="inputfield"><input type="number" min="0.00" max="1000000.00" step="0.01" class="lesSalaires input"></div</td><td><div class="inputfield"><div class="custom_select"><select class="lesUnites"><option value="EUR" selected>EUR</option><option value="FRF">FRF</option></select></div></div> </td<td><div class="revenuReel lesSalairesReels"></div></td></tr>';
     var tableau = document.getElementById('aRemplir');
